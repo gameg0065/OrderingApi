ed2e3b7abad11e86393fae2264043dc03a6353e8
Tomas <kvedaravicius2@gmail.com>
2021-03-11 22:52:34 +0200

    Saving and reading from file

--- orderingApi/Services/OrderNumbersService.cs
+++ orderingApi/Services/OrderNumbersService.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using OrderingApi.Services.Interfaces;
+using OrderingApi.Helpers;
 using System;
+using System.IO;
 
 namespace OrderingApi.Services
 {
@@ -14,14 +16,26 @@ namespace OrderingApi.Services
         public List<int> SortListOfNumbers(List<int> list)
         {
             Console.WriteLine("works List!!");
+            SaveToFile(list);
             return new List<int>(new int[] { 9, 9, 9 });
         }
 
 
         public List<int> GetLastResult()
         {
-            Console.WriteLine("works!!");
-            return new List<int>(new int[] { 8, 8, 8 });
+            return ReadFromFile();
+        }
+
+        private void SaveToFile(List<int> sortedList)
+        {
+            File.WriteAllTextAsync("result.txt", JsonHelper.CreateJsonString(sortedList));
+        }
+
+        private List<int> ReadFromFile()
+        {
+            var textFromFile = File.ReadAllText(@"result.txt");
+            var list = JsonHelper.DeserializeJsonToIntList(textFromFile);
+            return list;
         }
 
     }
