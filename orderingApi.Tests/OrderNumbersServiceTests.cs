47f1f717c3c16d353e8eba4c9d6ce697b275aef1
Tomas <kvedaravicius2@gmail.com>
2021-03-12 10:13:10 +0200

    More tests added

--- orderingApi.Tests/OrderNumbersServiceTests.cs
+++ orderingApi.Tests/OrderNumbersServiceTests.cs
@@ -22,5 +22,22 @@ namespace OrderingApi.Tests
 
             Assert.True(result, "Sorted value is not correct");
         }
+
+        [Fact]
+        public void SortStringOfNumbers2()
+        {
+            var input = "2 1 3 66 33 22 12 453 53 54 87 43";
+            var output = "1 2 3 12 22 33 43 53 54 66 87 453";
+
+            var orderingService = new OrderNumbersService(new SortingService(), new FileService());
+
+            string sortedValue = orderingService.SortListOfNumbers(input);
+
+            bool result = sortedValue == output;
+
+            Assert.True(result, "Sorted value is not correct");
+        }
+
+
     }
 }
--- orderingApi.Tests/SortingServiceTests.cs
+++ orderingApi.Tests/SortingServiceTests.cs
@@ -24,9 +24,42 @@ namespace OrderingApi.Tests
 
             Assert.True(result, "Sorted value is not correct !");
         }
+        [Fact]
+        public void BubbleSortTest2()
+        {
+            var input = new List<int>(new int[] { 2, 1, 3, 66, 33, 22, 12, 453, 53, 54, 87, 43 });
+            var output = new List<int>(new int[] { 1, 2, 3, 12, 22, 33, 43, 53, 54, 66, 87, 453 });
+
+            var sortingService = new SortingService();
+            var list = sortingService.SortWithBubbleSort(input);
+
+            var firstNotSecond = list.Except(output).ToList();
+            var secondNotFirst = output.Except(list).ToList();
+
+            bool result = !firstNotSecond.Any() && !secondNotFirst.Any();
+
+            Assert.True(result, "Sorted value is not correct !");
+        }
 
         [Fact]
         public void InsertionSortTest1()
+        {
+            var input = new List<int>(new int[] { 2, 1, 3 });
+            var output = new List<int>(new int[] { 1, 2, 3 });
+
+            var sortingService = new SortingService();
+            var list = sortingService.SortWithInsertionSort(input);
+
+            var firstNotSecond = list.Except(output).ToList();
+            var secondNotFirst = output.Except(list).ToList();
+
+            bool result = !firstNotSecond.Any() && !secondNotFirst.Any();
+
+            Assert.True(result, "Sorted value is not correct !");
+        }
+
+        [Fact]
+        public void InsertionSortTest2()
         {
             var input = new List<int>(new int[] { 2, 1, 3 });
             var output = new List<int>(new int[] { 1, 2, 3 });
@@ -60,10 +93,44 @@ namespace OrderingApi.Tests
         }
 
         [Fact]
+        public void ShellSortTest2()
+        {
+            var input = new List<int>(new int[] { 2, 1, 3, 66, 33, 22, 12, 453, 53, 54, 87, 43 });
+            var output = new List<int>(new int[] { 1, 2, 3, 12, 22, 33, 43, 53, 54, 66, 87, 453 });
+
+            var sortingService = new SortingService();
+            var list = sortingService.SortWithShellSort(input);
+
+            var firstNotSecond = list.Except(output).ToList();
+            var secondNotFirst = output.Except(list).ToList();
+
+            bool result = !firstNotSecond.Any() && !secondNotFirst.Any();
+
+            Assert.True(result, "Sorted value is not correct !");
+        }
+
+        [Fact]
         public void ShellQuickTest1()
         {
             var input = new List<int>(new int[] { 2, 1, 3 });
             var output = new List<int>(new int[] { 1, 2, 3 });
+
+            var sortingService = new SortingService();
+            var list = sortingService.SortWithQuickSort(input);
+
+            var firstNotSecond = list.Except(output).ToList();
+            var secondNotFirst = output.Except(list).ToList();
+
+            bool result = !firstNotSecond.Any() && !secondNotFirst.Any();
+
+            Assert.True(result, "Sorted value is not correct !");
+        }
+
+        [Fact]
+        public void ShellQuickTest2()
+        {
+            var input = new List<int>(new int[] { 2, 1, 3, 66, 33, 22, 12, 453, 53, 54, 87, 43 });
+            var output = new List<int>(new int[] { 1, 2, 3, 12, 22, 33, 43, 53, 54, 66, 87, 453 });
 
             var sortingService = new SortingService();
             var list = sortingService.SortWithQuickSort(input);
